040b4c1568ccdef4d3a430ccbfee6d843882363e
Marlon <61248995+bedimcode@users.noreply.github.com>
2022-05-07 10:13:19 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== LINK ACTIVE ===============*/
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active-link'))
+    this.classList.add('active-link')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+/*=============== SHOW HIDDEN MENU ===============*/
+const showMenu = (toggleId, navbarId) =>{
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId)
+
+    if(toggle && navbar){
+        toggle.addEventListener('click', ()=>{
+            /* Show menu */
+            navbar.classList.toggle('show-menu')
+            /* Rotate toggle icon */
+            toggle.classList.toggle('rotate-icon')
+        })
+    }
+}
+showMenu('nav-toggle','nav')
